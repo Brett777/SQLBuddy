3030535dee4eb656d0dc59ee7cba558ea8506b79
BrettOlmstead <brett.olmstead@datarobot.com>
2023-10-27 15:06:30 -0400

    My first commit

--- streamlitSQLBuddy.py
+++ streamlitSQLBuddy.py
@@ -5,11 +5,13 @@ import os
 import openai
 openai.api_key = os.getenv("OPENAI_KEY")
 
-testSql = """SELECT COUNT(*) 
-FROM DEMO.SAFER_LC.LENDING_CLUB_PROFILE;"""
+# snowflakeSQL = """-- This query will count the number of unique customers from California (CA)
+# SELECT COUNT(DISTINCT "CustomerID")
+# FROM "DEMO"."SAFER_LC"."LENDING_CLUB_PROFILE"
+# WHERE "addr_state" = 'CA';"""
 
 #Configure the page title, favicon, layout, etc
-st.set_page_config(page_title="SQL Buddy")
+st.set_page_config(page_title="Data Bot")
 
 def getSQL(queryDescription):
     #queryDescription = "how many rows are in the customer table?"
@@ -25,7 +27,8 @@ def getSQL(queryDescription):
                         Your response shall be the Snowflake SQL code to query the needed data. 
                         Your code shall be clean, concise and executable. 
                         Include comments to explain your code.
-                        Only respond with Snowflake SQL.
+                        Only respond with executable Snowflake SQL and nothing else. Anything other that Snowflake SQL must be commented. 
+                        When referencing column names, wrap them in double-quotes.                       
                              
                         Database name: DEMO
                         Schema: SAFER_LC
@@ -66,6 +69,25 @@ def getSQL(queryDescription):
     )
     return completion.choices[0].message.content
 
+def sayAnswer(query, answer):
+    completion = openai.ChatCompletion.create(
+        model="gpt-4",
+        # model="gpt-3.5-turbo",
+        temperature=0.2,
+        messages=[
+            {"role": "system",
+             "content": """
+                        Your job is to form a complete English sentence as an answer to the user's question.
+                        You will be provided with the user's question, as well as some data that supports the answer.
+                        Analyze the data, and provide a short answer in a sentence or two. 
+                            """
+             },
+            {"role": "user", "content": "question: " + query + " Supporting Data: " + answer}
+        ]
+    )
+    return completion.choices[0].message.content
+
+
 def executeSnowflakeQuery(snowflakeSQL):
     con = snowflake.connector.connect(
         user='DATAROBOT',
@@ -77,34 +99,41 @@ def executeSnowflakeQuery(snowflakeSQL):
     )
     cursor = con.cursor()
     cursor.execute(snowflakeSQL)
-    cursor.execute(testSql)
     results = cursor.fetchall()
-    df = pd.DataFrame(results, columns=[col[0] for col in cursor.description])
     con.close()
-    return df
+    return results
 
 def mainPage():
     container1 = st.container()
-    col1, col2, col3 = container1.columns([1,3,1])
     container2 = st.container()
     container3 = st.container()
+    if "generateQueryButtonState" not in st.session_state:
+        st.session_state["generateQueryButtonState"] = False
+        st.session_state["executeQueryButtonState"] = False
+        st.session_state["sqlQuery"] = ""
+        st.session_state["sqlQueryUserText"] = ""
+        st.session_state["queryResult"] = ""
 
-    with col2:
-        st.header("SQL Buddy")
+    with container1:
+        st.header("Data Bot")
 
     with container2:
-        plainEnghlishQuery = st.text_input(label="Describe the query you want to make.")
-        generateQueryButton = st.button("Generate SQL")
+        plainEnghlishQuery = st.text_input(label="Ask a question about the data")
+        generateQueryButton = st.button("Get Answer")
         if generateQueryButton:
-            sqlQuery = getSQL(plainEnghlishQuery)
+            st.session_state["generateQueryButtonState"] = True
+            with st.spinner("Generating Query..."):
+                st.session_state["sqlQuery"] = getSQL(plainEnghlishQuery)
+
+            with container3:
+                st.code(st.session_state["sqlQuery"], language="sql")
+                with st.spinner("Executing Query..."):
+                    st.session_state["queryResult"] = executeSnowflakeQuery(str(st.session_state["sqlQuery"]))
+                    st.write(st.session_state["queryResult"])
+                    st.write(sayAnswer(str(plainEnghlishQuery), str(st.session_state["queryResult"])))
 
 
-    with container3:
-        sqlQueryUserText = st.text_area(label="Here is your SQL Query", value=sqlQuery)
-        executeQueryButton = st.button("Execute Query")
-        if executeQueryButton:
-            df = executeSnowflakeQuery(sqlQuery)
-            st.dataframe(df)
+
 
 
 
