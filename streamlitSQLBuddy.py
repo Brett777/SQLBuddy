965e0ea9126a2d929c9d1c837fc317146e01c33d
BrettOlmstead <brett.olmstead@datarobot.com>
2023-10-31 13:32:04 -0400

    My first commit

--- streamlitSQLBuddy.py
+++ streamlitSQLBuddy.py
@@ -118,6 +118,11 @@ def mainPage():
         st.write("Ask a question about the Lending Club dataset in plain language.")
         st.write("The question will be transformed into a query, executed and the result set returned.")
         st.write("Finally, the answer will be provided in sentence form.  ")
+        st.write("Examples of questions you could try:")
+        st.caption("What is the average term of a bad loan?")
+        st.caption("Which customer had the most transactions and a bad loan?")
+        st.caption("How many transactions did C900001946 have?")
+        st.caption("Do we make more money on short term or long term loans?")
 
     with container2:
         plainEnghlishQuery = st.text_input(label="Your question", value="How many customers from California had a bad loan?")
